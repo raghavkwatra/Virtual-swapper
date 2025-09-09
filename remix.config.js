b85a7fa1f13f48df9cce5acca803bd8a60284d39
Raghav Kwatra <160281251+raghavkwatra@users.noreply.github.com>
2025-09-10 02:03:46 +0530

    feat: Prepare for deployment and serverless execution
    Updates the build script to correctly output JavaScript files to the `public` directory, enabling static asset serving. Removes the unused Remix configuration to streamline the project for serverless deployment.

--- remix.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('@remix-run/dev').AppConfig} */
-export default {
-  // This is a placeholder file to satisfy Netlify's build detection.
-  // The project does not actually use Remix.
-};
